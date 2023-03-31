80dcd21df990ebe6393057814bef8667a4b009e9
Rajitha <rajithat1998@gmail.com>
2023-03-31 15:58:58 +0530

    update

--- Server/sever.js
+++ Server/sever.js
@@ -1,10 +1,43 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+
+dotenv.config({ path: './config.env' });
+
+// check if DATABASE environment variable is defined
+if (!process.env.DATABASE) {
+  console.log('DATABASE environment variable is not defined');
+  process.exit(1);
+}
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true
+  })
+  .then(() => console.log('DB Connection Successful'))
+  .catch((err) => console.error('Error connecting to database:', err));
+
+const port = process.env.PORT;
+
+//log middleware function
+//add id to middleware
+function log(req,res,next) { 
+  //add feild to the  request usage to log auth
+  req.id = "10";
+  next();
+}
 
 //use log middleware
 //first check middleware
-app.use(log)
+app.use(log);
 
 //use morgon middleware
 app.use(morgan('tiny'));
@@ -19,19 +52,10 @@ app.use(express.json());
 
 //re route all api/users route to users 
 //adn check log middleware
-//app.use('/api/users', log, user);
 app.use('/api/users', user);
 app.use('/api/me',home);
- 
-//log middleware function
-//log mean middlewate
-//add id to middleware
-function log(req,res,next) { 
-    // console.log("Printed");
-   //add feild to the  request usage to log auth
-    req.id = "10";
-    next();
-}
 
-const port = process.env.PORT || 3000;
-app.listen(port,() => console.log(`App is running on port ${port}`));
+//app.listen(port,() => console.log(`App is running on port ${port}`));
+const server = app.listen(port, () => {
+  console.log('Listening to port ' + port);
+});
